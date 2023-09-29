cd59c35cff70d1db1883449286ec2e85c9d82c29
Sadab Ali <84293105+sadabjr@users.noreply.github.com>
2023-09-30 00:08:12 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import ContactList from './components/ContactList'
+// import ContactList from './components/ContactList'
 import Contact from './components/contact'
 
 
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <Contact/>
-      <ContactList/>
+      {/* <ContactList/> */}
     </>
   )
 }
